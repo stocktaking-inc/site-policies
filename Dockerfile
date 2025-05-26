43488bc6168cd925a7f7c7376598fbc1361f37a4
Daniil Oberlev <denioberlev@gmail.com>
2025-05-26 07:58:31 +0300

    feat(docker): add docker bundle

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:18-alpine AS builder
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm ci
+COPY . .
+RUN npm run build
+
+FROM node:18-alpine AS runner
+WORKDIR /app
+RUN npm install -g http-server
+COPY --from=builder /app/dist ./dist
+RUN chmod -R 755 /app/dist
+EXPOSE 5432
+CMD ["http-server", "dist", "-p", "5432", "-a", "0.0.0.0", "--gzip", "--brotli"]
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
-// @ts-check
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import compressor from 'astro-compressor';
 
 export default defineConfig({
   site: 'https://site-policies.vercel.app',
+  outDir: './dist',
+  output: 'static',
+
   integrations: [
     sitemap({
       changefreq: 'weekly',
       priority: 0.7,
       lastmod: new Date(),
       filter: page => true
-    })
+    }),
+    compressor(),
   ]
 })
